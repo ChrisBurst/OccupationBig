4443c5e7dbef8bc6eebb1b0be84c396b814829f7
ChrisBurst <chrisburst@comcast.net>
2015-03-16 13:51:05 -0400

    Added Prototype as base

---
+++ Assets/Controller.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	//adjustable speed of player
+	public float speed;
+	public float size=1;
+
+	//Xpos and Zpos do not move Player, but keep track of its location
+	public float Xpos;
+	public float Zpos;
+
+	private float TempSpeed=0;
+
+
+	void OnTriggerEnter(Collider other){
+		print (other.gameObject.name + " is touching "+gameObject.name);
+
+		//Lava is a killzone
+		if (other.gameObject.tag=="Lava") {
+			Destroy (gameObject);
+			print ("You BURN.");
+		}
+
+		//Thorn bushes slow down a dino. The bigger they are, the more they slow.
+		if (other.gameObject.tag == "Thorn") {
+			TempSpeed=speed; //Store a variable for what the speed should go back to on exit.
+			speed = speed*(.5f/size);
+			print ("Speed reduced to "+speed);
+		}
+
+		//Tall Grass hides smaller dinos. Exact size limit not determined
+		if (other.gameObject.tag == "Grass") {
+			if(size<4){ //NOTE: 5 is placeholder number
+				print ("You are Hidden");
+			}else{
+				print ("You are too large to be hidden");
+			}
+		}
+
+		if (other.gameObject.tag == "NPCDino") {
+			Destroy(other.gameObject);
+			print ("You eat the dino.");
+			transform.localScale+= new Vector3(1f,0,1f);
+			size++;
+		}
+	}
+
+	//Undo effects of terrain once player leaves
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.tag == "Thorn") {
+			speed=TempSpeed; //return speed to normal.
+			print ("Speed back to "+speed);
+		}
+		if (other.gameObject.tag == "Grass") {
+			print ("Out of grass, no longer hidden");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			if(Xpos>0){//Don't move if on the border
+				transform.Translate (-speed, 0, 0);//move
+				Xpos-=speed;//Keep track of location
+			}//NOTE: Boundary code only activates if player is already out of bounds.
+			//It does not keep them within the boundaries
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			if (Xpos < 50) {
+				transform.Translate (speed, 0, 0);
+				Xpos += speed;
+			}
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			if(Zpos>0){
+				transform.Translate (0, 0, -speed);
+				Zpos-=speed;
+			}
+		}
+		if (Input.GetKey (KeyCode.RightArrow)){
+			if (Zpos < 50) {
+				transform.Translate (0, 0, speed);
+				Zpos += speed;
+			}
+		}
+
+		//DEBUG ABILITY
+		if (Input.GetKey (KeyCode.G)) {
+			print ("AWESOME!");
+			//Placeholder until we have something we want to alter during testing
+		}
+
+	}
+}
---
+++ Assets/NPC.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour {
+
+	//how long before NPC chooses a new direction to move
+	public float wanderLength;
+	public float speed;
+	public float size=1;
+
+	//MUST match object location
+	public float Xstart;
+	public float Zstart;
+
+	//keeps track of location
+	private float Xpos=0;
+	private float Zpos=0;
+
+	private int counter=0;
+	private int direction=0;
+	/*directions are clockwise
+	 * 812
+	 * 7-3
+	 * 654*/
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		Xpos = Xstart;
+		Zpos = Zstart;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Wander ();
+
+	}
+
+	void Wander(){
+		//After [WanderLength] turns, a new direction is randomly selected
+		if (counter % wanderLength == 0){
+			direction = Random.Range (1, 8);
+		}
+		//Counts how many turns have gone by
+		counter++;
+
+
+		if ((direction==8)||(direction==1)||(direction==2)) {
+			if (Xpos>0){//Don't move if on the border
+				transform.Translate (-speed, 0, 0);//move
+				Xpos-=speed;//Keep track of location
+			}//NOTE: Boundary code only activates if player is already out of bounds.
+			//It does not keep them within the boundaries
+		}
+		if (direction==4||direction==5||direction==6) {
+			if (Xpos < 50) {
+				transform.Translate (speed, 0, 0);
+				Xpos += speed;
+			}
+		}
+		if (direction==6||direction==7||direction==8) {
+			if(Zpos>0){
+				transform.Translate (0, 0, -speed);
+				Zpos-=speed;
+			}
+		}
+		if (direction==2||direction==3||direction==4){
+			if (Zpos < 50) {
+				transform.Translate (0, 0, speed);
+				Zpos += speed;
+			}
+		}
+	}
+}
